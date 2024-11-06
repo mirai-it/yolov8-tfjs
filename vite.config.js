dcb8edbb7d2f48a5eb671d0e97746855fd6d3aa7
mirai-it <mirai.itsys+kaken@gmail.com>
2024-11-06 13:48:19 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 2000, // handle warning on vendor.js bundle size
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   },
   base: "/yolov8-tfjs/",
 });
